58740847e8702da6a06fdbd090c4b9967786bd01
Nha <nguyenhoangvannha@gmail.com>
2018-01-28 16:06:13 +0700

    Learn core java

---
+++ JavaDeepDive/Variables.java
@@ -0,0 +1,14 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+/**
+ * Variables
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        List list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+    }
+}
