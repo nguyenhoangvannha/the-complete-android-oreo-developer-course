3d3a9845cab70772f88bbf367f375e434885f7ba
Nha <nguyenhoangvannha@gmail.com>
2018-01-28 17:16:46 +0700

    Animations: Fade, Translation, Rotate, Scale,

--- Animations/app/src/main/java/com/example/nhvn/animations/MainActivity.java
+++ Animations/app/src/main/java/com/example/nhvn/animations/MainActivity.java
@@ -12,21 +12,32 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Info", "ImageView tapped!");
         ImageView imageView = findViewById(R.id.imageView);
         ImageView imageView2 = findViewById(R.id.imageView2);
-        if(switcher == true){
-            imageView.animate().alpha(0).setDuration(2000);
-            imageView2.animate().alpha(1).setDuration(2000);
-            switcher = false;
-        } else {
-            imageView.animate().alpha(1).setDuration(2000);
-            imageView2.animate().alpha(0).setDuration(2000);
-            switcher = true;
-        }
-
+//        if(switcher == true){
+//            imageView.animate().alpha(0).setDuration(2000);
+//            imageView2.animate().alpha(1).setDuration(2000);
+//            switcher = false;
+//        } else {
+//            imageView.animate().alpha(1).setDuration(2000);
+//            imageView2.animate().alpha(0).setDuration(2000);
+//            switcher = true;
+//        }
+        //imageView.animate().translationYBy(1000).setDuration(2000);
+        //imageView.animate().translationXBy(-1000).setDuration(2000);
+        imageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(1000);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startApp();
+    }
+
+    private void startApp() {
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setScaleX(0);
+        imageView.setScaleY(0);
+        imageView.setX(-500);
+        imageView.animate().translationXBy(500).alpha(1).scaleX(1).scaleY(1).setDuration(10000);
     }
 }
