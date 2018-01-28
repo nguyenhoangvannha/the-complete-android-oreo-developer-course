544ea9d1f8c40faf4a4bb78f0ba078772365a802
Nha <nguyenhoangvannha@gmail.com>
2018-01-28 22:03:45 +0700

    Sound demo with seekbar

---
+++ SoundDemo/app/src/main/java/com/example/nhvn/sounddemo/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.nhvn.sounddemo;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isPlaying = false;
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mediaPlayer = MediaPlayer.create(this, R.raw.laugh);
+        SeekBar volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        volumeSeekBar.setProgress(currentVolume);
+        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        final SeekBar scrubSeekBar = findViewById(R.id.scrubSeekBar);
+        int audioLength = mediaPlayer.getDuration();
+        scrubSeekBar.setMax(audioLength);
+        scrubSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mediaPlayer.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                scrubSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);
+    }
+    public void playSound(View view){
+        if(!isPlaying){
+            mediaPlayer.start();
+            isPlaying = true;
+        }
+    }
+    public void pauseSound(View view){
+        if(isPlaying){
+            mediaPlayer.pause();
+            isPlaying = false;
+        }
+    }
+}
