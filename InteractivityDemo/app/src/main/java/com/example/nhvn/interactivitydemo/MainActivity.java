db76d6cb11d3bf020b849087dd810935c77c113e
Nha <nguyenhoangvannha@gmail.com>
2018-01-28 10:00:11 +0700

    Exercise Button

--- InteractivityDemo/app/src/main/java/com/example/nhvn/interactivitydemo/MainActivity.java
+++ InteractivityDemo/app/src/main/java/com/example/nhvn/interactivitydemo/MainActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
     public  void  clickFunction(View view){
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
         Log.i("Info","Hello World!");
+        Log.i("Values", nameEditText.getText().toString());
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
