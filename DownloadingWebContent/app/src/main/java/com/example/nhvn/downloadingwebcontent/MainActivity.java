b9062665ea5b6f0648ecb1640a3310ad1b672fb1
Nha <nguyenhoangvannha@gmail.com>
2018-01-30 09:09:45 +0700

    Download html from a web page

---
+++ DownloadingWebContent/app/src/main/java/com/example/nhvn/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.nhvn.downloadingwebcontent;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data != - 1){
+                    char current = (char) data;
+                    result += current;
+
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (IOException e) {
+                return "Failed";
+            }
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+        }
+
+        DownloadTask task = new DownloadTask();
+        String result = "null";
+        try{
+            result = task.execute("http://www.zappycode.com/").get();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+    }
+}
